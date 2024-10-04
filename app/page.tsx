0dad55fb7d82fcc79ee0da0520c17b17b5120854
gostshell <sevengoldn@gmail.com>
2024-10-04 18:13:06 +0300

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -164,14 +164,9 @@ export default function Home() {
            </div>
 
           <div className="px-4 mt-4 flex justify-center">
-              <div className="w-50 h-50 p-4 rounded-full circle-outer">
+              <div className="w-50 h-50 p-4 rounded-full circle-outer" onClick={handleIncreasePoints}>
                 <div className="w-full h-full rounded-full circle-inner">
-                <button
-            onClick={handleIncreasePoints}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
-            >
-             Points
-             </button>
+                
 
                  
                 </div>
